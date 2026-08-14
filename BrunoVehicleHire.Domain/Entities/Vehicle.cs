efc5db2315837b6568085383d8e4b99ee811c477
kylemarshalldev <kylem@agilebridge.co.za>
2026-08-14 15:29:27 +0200

    prevowned not relevant for hire

--- BrunoVehicleHire.Domain/Entities/Vehicle.cs
+++ BrunoVehicleHire.Domain/Entities/Vehicle.cs
@@ -9,14 +9,17 @@ public class Vehicle
     public int Year { get; private set; }
     public double Mileage { get; private set; }
     public VehicleColour Colour { get; private set; }
-    public bool PreviouslyOwned { get; private set; }
     public ServiceHistory ServiceHistory { get; private set; }
+    public string SellerName { get; private set; } = string.Empty;
+    public decimal PricePerDay { get; private set; }
+    public DateTime? HireStartDate { get; private set; }
+    public DateTime? HireEndDate { get; private set; }
     public bool IsDeleted { get; private set; }
     public DateTime CreatedDate { get; private set; }
 
     private Vehicle() { }
 
-    public Vehicle(string registrationNumber, string make, string model, int year, double mileage, VehicleColour colour, bool previouslyOwned, ServiceHistory serviceHistory)
+    public Vehicle(string registrationNumber, string make, string model, int year, double mileage, VehicleColour colour, ServiceHistory serviceHistory, string sellerName, decimal pricePerDay, DateTime? hireStartDate, DateTime? hireEndDate)
     {
         Id = Guid.NewGuid();
         RegistrationNumber = registrationNumber;
@@ -25,21 +28,27 @@ public class Vehicle
         Year = year;
         Mileage = mileage;
         Colour = colour;
-        PreviouslyOwned = previouslyOwned;
         ServiceHistory = serviceHistory;
+        SellerName = sellerName;
+        PricePerDay = pricePerDay;
+        HireStartDate = hireStartDate;
+        HireEndDate = hireEndDate;
         IsDeleted = false;
         CreatedDate = DateTime.UtcNow;
     }
 
-    public void Update(string make, string model, int year, double mileage, VehicleColour colour, bool previouslyOwned, ServiceHistory serviceHistory)
+    public void Update(string make, string model, int year, double mileage, VehicleColour colour, ServiceHistory serviceHistory, string sellerName, decimal pricePerDay, DateTime? hireStartDate, DateTime? hireEndDate)
     {
         Make = make;
         Model = model;
         Year = year;
         Mileage = mileage;
         Colour = colour;
-        PreviouslyOwned = previouslyOwned;
         ServiceHistory = serviceHistory;
+        SellerName = sellerName;
+        PricePerDay = pricePerDay;
+        HireStartDate = hireStartDate;
+        HireEndDate = hireEndDate;
     }
 
     public void SoftDelete()
